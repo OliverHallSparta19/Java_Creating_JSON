6516b5765a82adcab7a97636bbcad9b39143b761
Oliver <OHall@spartaglobal.com>
2018-11-05 14:46:22 +0000

    First Name Length Test Working

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,20 +1,28 @@
+import org.json.simple.JSONArray;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class Tests {
 
+    UserProfile testUser;
 
-
-    @BeforeClass
-    private static setClass(){
-
+    @Before
+    public void Setup(){
+        JSONArray hobbies = new JSONArray();
+        hobbies.add("jujitsie");
+        hobbies.add("soccer");
+        testUser = new UserProfile("Geoff",45,"geoff@mail.com", hobbies);
     }
 
 
-    @Before
+
 
 
     @Test
-
+    public void TestNameString(){
+        Assert.assertEquals(5, testUser.getName().length());
+        System.out.println("Name length is " + testUser.getName().length());
+    }
 }
